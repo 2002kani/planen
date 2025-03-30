4a5626e9cae467f338b51e30eee1588963ec28cf
02Kani <kanikaradag02@gmail.com>
2025-03-30 20:57:10 +0200

    #

--- src/Components/TaskFormPopup.tsx
+++ src/Components/TaskFormPopup.tsx
@@ -1,17 +1,30 @@
-import type { PropsWithChildren } from "react"
+import { useState, type PropsWithChildren } from "react"
+import { useLocation } from "react-router"
 
 import { Dialog, DialogContent, DialogTrigger } from "@/Components/ui/dialog"
 import TaskForm from "./TaskForm"
+import { startOfToday } from "date-fns"
 
 const TaskFormPopup: React.FC<PropsWithChildren> = ({ children }) => {
+
+  const location = useLocation();
+  const [open, setOpen] = useState(false);
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
             {children}
         </DialogTrigger>
 
         <DialogContent className="p-0 border-0 rounded-xl">
-            <TaskForm mode="create" />
+            <TaskForm defaultFormData={{
+              content: "",
+              due_date: location.pathname === "/app/today" ? startOfToday() : null,
+              projectId: null,
+              }}
+              mode="create"
+              onCancel={() => setOpen(false)}
+              />
         </DialogContent>
     </Dialog>
   )
