6638abdb05c2b80687f4a68590c95f324dd54e36
02Kani <kanikaradag02@gmail.com>
2025-04-03 20:15:19 +0200

    image updated

--- src/Assets/index.ts
+++ src/Assets/index.ts
@@ -3,10 +3,10 @@ import logo from "@/Assets/logo.png"
 import heroBannerSm from '@/assets/hero-banner-sm.png';
 import heroBannerLg from '@/assets/hero-banner-lg.png';
 import todayTaskEmptyState from '@/assets/today-task-empty-state.png';
-import inboxTaskEmptyState from '@/assets/inbox-task-empty-state.png';
 import upcomingTaskEmptyState from '@/assets/upcoming-task-empty-state.png';
 import completedTaskEmptyState from '@/assets/completed-task-empty-state.png';
 import projectTaskEmptyState from '@/assets/project-task-empty-state.png';
 import pageNotFound from '@/assets/page-not-found.png';
+import inBoxBlueEmptyState from "@/assets/inbox-task-blue-shirt (1).png"
 
-export { logo, heroBannerSm, heroBannerLg, todayTaskEmptyState, inboxTaskEmptyState, upcomingTaskEmptyState, completedTaskEmptyState, projectTaskEmptyState, pageNotFound };
+export { logo, heroBannerSm, heroBannerLg, todayTaskEmptyState, inBoxBlueEmptyState, upcomingTaskEmptyState, completedTaskEmptyState, projectTaskEmptyState, pageNotFound };
