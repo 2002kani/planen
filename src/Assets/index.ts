ee0acaa46e22c858489eca25f039c768e46c7af0
02Kani <kanikaradag02@gmail.com>
2025-04-07 16:21:53 +0200

    pic added

--- src/Assets/index.ts
+++ src/Assets/index.ts
@@ -3,7 +3,7 @@ import logo from "@/Assets/logo.png"
 import heroBannerSm from '@/assets/hero-banner-sm.png';
 import heroBannerLg from '@/assets/hero-banner-lg.png';
 import todayTaskEmptyState from '@/assets/Image 6. Apr. 2025, 22_44_30 (1).png';
-import upcomingTaskEmptyState from '@/assets/Image 6. Apr. 2025, 22_50_24.png';
+import upcomingTaskEmptyState from '@/assets/Image_6._Apr._2025__22_50_24-removebg-preview.png';
 import completedTaskEmptyState from '@/assets/completed-task-empty-state.png';
 import projectTaskEmptyState from '@/assets/project-task-empty-state.png';
 import pageNotFound from '@/assets/page-not-found.png';
