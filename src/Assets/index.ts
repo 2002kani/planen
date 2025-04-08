05de0b558c016aa4788c3b40ed4ddf4ecf6599bf
02Kani <kanikaradag02@gmail.com>
2025-04-08 22:47:21 +0200

    #

--- src/Assets/index.ts
+++ src/Assets/index.ts
@@ -6,7 +6,7 @@ import todayTaskEmptyState from '@/assets/Image 6. Apr. 2025, 22_44_30 (1).png';
 import upcomingTaskEmptyState from '@/assets/Image_6._Apr._2025__22_50_24-removebg-preview.png';
 import completedTaskEmptyState from '@/assets/Image_7._Apr._2025__18_00_35-removebg-preview.png';
 import projectTaskEmptyState from '@/assets/project-task-empty-state.png';
-import pageNotFound from '@/assets/page-not-found.png';
+import pageNotFound from '@/assets/Image_8._Apr._2025__22_42_16-removebg-preview.png';
 import inBoxBlueEmptyState from "@/assets/inbox-task-blue-shirt (1).png"
 
 export { logo, heroBannerSm, heroBannerLg, todayTaskEmptyState, inBoxBlueEmptyState, upcomingTaskEmptyState, completedTaskEmptyState, projectTaskEmptyState, pageNotFound };
