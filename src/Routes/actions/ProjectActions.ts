576a2e7e181e9e20a5b79a27a01a3e3bc97273ab
02Kani <kanikaradag02@gmail.com>
2025-04-12 19:04:51 +0200

    #

--- src/Routes/actions/ProjectActions.ts
+++ src/Routes/actions/ProjectActions.ts
@@ -1,31 +1,44 @@
 import type { ActionFunction } from "react-router";
+import { redirect } from "react-router";
+import { Models } from "appwrite";
 
 import { databases } from "@/lib/appwrite";
 import { generateID, getUserId } from "@/Service/appActionHelper";
 
-import { Project } from "@/Types/typesIndex";
+import { ProjectForm } from "@/Types/typesIndex";
 
 const APPWRITE_DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 
-const createProjects = async (data: Project) => {
+const createProject = async (data: ProjectForm) => {
+    let project: Models.Document | null = null;
+    const aiTaskGen = data.ai_task_gen;
+    const taskGenPrompt = data.task_gen_prompt;
+
     try{
-        return await databases.createDocument(
+        project = await databases.createDocument(
             APPWRITE_DATABASE_ID,
             "projects",
             generateID(),
-            { ...data, id: getUserId() }
+            {
+                name: data.name,
+                color_name: data.color_name,
+                color_hex: data.color_hex,
+                userId: getUserId()
+            }
         );
     }catch(err){
         console.log(err);  
     }
+
+    return redirect(`app/projects/${project?.$id}`);
 }
 
 const ProjectActions: ActionFunction = async ({ request }) => {
-    const data = await request.json() as Project;
+    const data = await request.json() as ProjectForm;
 
     if(request.method === "POST"){
-        return await createProjects(data);
-    }
+        return await createProject(data);
+    } 
 }  
 
 export default ProjectActions
