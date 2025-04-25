e0e975f9af781dcadf368522e01ab734bab90b92
02Kani <kanikaradag02@gmail.com>
2025-04-25 20:33:30 +0200

    #

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import router from './Routes/index.tsx'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 const SIGN_IN_FORCE_REDIRECTED_URL = import.meta.env.VITE_CLERK_SIGN_IN_FORCE_REDIRECTED_URL
 const SIGN_UP_FORCE_REDIRECTED_URL = import.meta.env.VITE_CLERK_SIGN_UP_FORCE_REDIRECTED_URL
-
+ 
 if(!PUBLISHABLE_KEY){
   throw new Error("Kein Clerk Key veröffentlicht");
 }
