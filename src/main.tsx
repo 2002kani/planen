ec184e6b2b40f2bb3c7db336ac1b723e99d91966
02Kani <kanikaradag02@gmail.com>
2025-05-02 23:42:18 +0200

    #

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,13 @@ import { RouterProvider } from 'react-router'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { dark } from '@clerk/themes'
 import { HelmetProvider } from 'react-helmet-async'
- 
+  
 import router from './Routes/index.tsx'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 const SIGN_IN_FORCE_REDIRECTED_URL = import.meta.env.VITE_CLERK_SIGN_IN_FORCE_REDIRECTED_URL
 const SIGN_UP_FORCE_REDIRECTED_URL = import.meta.env.VITE_CLERK_SIGN_UP_FORCE_REDIRECTED_URL
-  
+   
 if(!PUBLISHABLE_KEY){
   throw new Error("Kein Clerk Key veröffentlicht");
 }
